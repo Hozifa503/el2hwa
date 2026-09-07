e78af738dc276e2e828962690cd279c3e6ca0616
Hozifa503 <prog.hozifa@gmail.com>
2026-09-07 16:15:15 +0300

    implement JavaScript selectors and DOM element references for reservation form inputs

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const persons = document.getElementById("persons");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const message = document.getElementById("message");
+
