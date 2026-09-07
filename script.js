dbd1be98e118a39b627d44afe844978cb3bb2262
Hozifa503 <prog.hozifa@gmail.com>
2026-09-08 02:25:24 +0300

    integrate EmailJS for real time table reservations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("contact-form");
+const contactForm = document.getElementById("contact-form");
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const persons = document.getElementById("persons");
@@ -6,3 +6,19 @@ const date = document.getElementById("date");
 const time = document.getElementById("time");
 const message = document.getElementById("message");
 
+contactForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const serviceID = "service_8dv10vy"; 
+    const templateID = "template_2451hxi";
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            console.log("SUCCESS! Table Hacked & Email Sent.");
+            alert("Reservation sent successfully!");
+            contactForm.reset();
+        }, (error) => {
+            console.log("FAILED...", error);
+            alert("Failed to send reservation, please try again.");
+        });
+});
